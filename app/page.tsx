3ed66b26bc57c6452e0e601cf69159bb44f46e29
itauge <du-qian@exp-corp.com>
2025-01-20 17:11:47 +0900

    clerk init

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,28 @@
+import {
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
 import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main className="flex flex-col justify-center h-screen max-w-5xl mx-auto">
       <h1 className="text-5xl font-bold">Invoicing App</h1>
+
       <p>
         <Button>
           <Link href="/dashboard">Sign in</Link>
         </Button>
+        <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+
       </p>
     </main>
   );
