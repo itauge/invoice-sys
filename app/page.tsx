be5955261ef819b79d8bc02cbc2af0a1d7684746
itauge <du-qian@exp-corp.com>
2025-01-22 16:09:56 +0900

    final tune

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,19 @@
 
 import { Button } from "@/components/ui/button";
 import Link from 'next/link';
+import { auth } from "@clerk/nextjs/server";
 
-export default function Home() {
+export default async function Home() {
+
+  const { userId } = await auth();
+
+
   return (
-      <main className="flex flex-col justify-center h-screen">
+      <main className="flex flex-col justify-center gap-2 h-screen items-center">
         <h1 className="text-5xl font-bold">Invoicing App</h1>
         <div className="flex gap-2">
-          <Button>
-            <Link href="/dashboard">Sign in</Link>
+          <Button className="w-full">
+            {userId ? <Link href="/dashboard">Dashboard</Link> : <Link href="/sign-in">Sign in</Link>}
           </Button>
         </div>
       </main>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,23 @@
 import {
-    SignInButton,
     SignedIn,
-    SignedOut,
     UserButton
   } from '@clerk/nextjs'
 import Container from './Container';
+import Link from 'next/link';
 
 const Header = () => {
+    
     return (
         <header>
             <Container>
-                <div className="flex justify-between items-center gap-4">
-                    <SignedOut> 
-                        <SignInButton />
-                    </SignedOut>
+            <div className="flex justify-between items-center w-full h-10">
                     <SignedIn>
+                        <Link 
+                            href="/dashboard" 
+                            className="text-sm font-medium text-muted-foreground hover:text-primary transition-colors"
+                        >
+                            Dashboard
+                        </Link>
                         <UserButton />
                     </SignedIn>
                 </div>
