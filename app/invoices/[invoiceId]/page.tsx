c763b6537024949302723b1f6ab8b148e288a9c6
itauge <du-qian@exp-corp.com>
2025-01-22 11:07:01 +0900

    add dropdown component, implement status change functionality

--- app/invoices/[invoiceId]/page.tsx
+++ app/invoices/[invoiceId]/page.tsx
@@ -1,13 +1,50 @@
+import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 import prisma from "@/prisma/db"
 import { cn } from "@/lib/utils";
 import Container from "@/components/Container";
 import { auth } from "@clerk/nextjs/server";
+import { ChevronDown } from "lucide-react";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+  } from "@/components/ui/dropdown-menu"
+import { Status } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+  
 
 interface Props {
     params: {
         invoiceId: string
     }
+}
+
+const AVAILABLE_STATUSES = [{id: "PENDING", label: "Pending"}, {id: "PAID", label: "Paid"}, {id: "CANCELLED", label: "Cancelled"}]
+// Another way to update the status instead of using api
+async function updateInvoiceStatus(formData: FormData) {
+    "use server"
+
+    const session = await auth();
+    
+    if (!session?.userId) {
+        throw new Error("User not found");
+    }
+
+    const id = formData.get("id") as string
+    const status = formData.get("status") as string
+
+    if (!["PENDING", "PAID", "CANCELLED"].includes(status)) {
+        throw new Error("Invalid status value");
+    }
+
+    await prisma.invoices.update({
+        where: {id: parseInt(id), userId: session.userId},
+        data: {status: status as Status}
+    })
+
+    revalidatePath(`/invoices/${id}`)
 }
 
 export default async function InvoicePage({params}: Props) {
@@ -33,7 +70,7 @@ export default async function InvoicePage({params}: Props) {
     }
 
   return (
-    <main className="h-full my-12">
+    <main className="w-full h-full my-12">
         <Container>
         <div className="flex justify-between mb-8">
         <h1 className="flex items-center gap-2 text-3xl font-semibold">{invoiceId}
@@ -46,9 +83,29 @@ export default async function InvoicePage({params}: Props) {
                 {invoice?.status}
             </Badge>
         </h1>
-        <p>
-            
-        </p>
+
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button className="flex items-center gap-2" variant="outline">
+                    Change Status
+                    <ChevronDown className="w-4 h-auto" />
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                {AVAILABLE_STATUSES.map((status) => (
+                    <DropdownMenuItem key={status.id}>
+                        <form action={updateInvoiceStatus}>
+                            <input type="hidden" name="id" value={invoice.id} />
+                            <input type="hidden" name="status" value={status.id} />
+                            <button type="submit" name="status" value={status.id}>
+                                {status.label}
+                            </button>
+                        </form>
+                    </DropdownMenuItem>
+                ))}
+            </DropdownMenuContent>
+        </DropdownMenu>
+
         </div>
         <p className="text-3xl mb-3">
             {invoice?.value}
